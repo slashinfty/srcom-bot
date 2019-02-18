fe321d2cb48c6b5f4e2d1ff20805d51e59f8d9f8
Matt Braddock <matt.braddock@gmail.com>
2019-02-18 16:17:12 -0500

    logging server count

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ for (const file of commandFiles) {
 client.once('ready', () => {
 	console.log('Ready!');
     client.user.setActivity("!src -help");
+    console.log('Bot is in ' + client.guilds.length + ' servers!');
     client.guilds.forEach(guild => console.log(guild.name));
 });
 
