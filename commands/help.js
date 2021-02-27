196d5f8a863a6ebfec79353320800a5841299146
Matt Braddock <matt.braddock@gmail.com>
2021-02-27 07:31:13 -0500

    domain update

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'help',
 	description: 'DM the user a help block',
 	execute: function (message, args) {
-        message.author.send("Hi! I can get information from speedrun.com and racetime.gg\n\nIf you need to see what commands to use, check this out: <https://mattbraddock.com/srcom-bot/>\n\nIf you want to report an issue, you can do that: <https://github.com/slashinfty/srcom-bot/issues>");
+        message.author.send("Hi! I can get information from speedrun.com and racetime.gg\n\nIf you need to see what commands to use, check this out: <https://slashinfty.github.io/srcom-bot/>\n\nIf you want to report an issue, you can do that: <https://github.com/slashinfty/srcom-bot/issues>");
 	}
 };
